0d5b815107b7f2987772c052f591ea091ac1c026
manar25-isg <manarsellami99@gmail.com>
2025-01-23 13:12:02 +0000

    crud utilisateurs

--- admin/services/liste.php
+++ admin/services/liste.php
@@ -42,19 +42,19 @@
                                                 $con = mysqli_connect('localhost','root','','db_events');
                                                 $select = "SELECT * FROM services";
                                                 $results = mysqli_query($con, $select);
-                                                foreach ($results as $service) {
+                                                foreach ($results as $services) {
                                                     ?>
 
-<tr>
-                                                <td><?= $service['image'] ?></td>
-                                                <td><?= $service['libelle'] ?></td>
-                                                <td><?= $service['description'] ?></td>
+                                                <tr>
+                                                <td><?= $services['image'] ?></td>
+                                                <td><?= $services['libelle'] ?></td>
+                                                <td><?= $services['description'] ?></td>
                                                 <td>
-                                                    <a href="../services/supprimer.php?id=<?= $service['id'] ?>" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-dash-fill" viewBox="0 0 16 16">
+                                                    <a href="../services/supprimer.php?id=<?= $services['id'] ?>" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-dash-fill" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M11 7.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5"/>
   <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
 </svg></a>
-                                                    <a href="../services/modifier.php?id=<?= $service['id'] ?>" class="btn btn-warning"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                                    <a href="../services/modifier.php?id=<?= $services['id'] ?>" class="btn btn-warning"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
   <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
 </svg></a>
                                                 </td>
